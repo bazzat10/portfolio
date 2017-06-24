9b223d1aa5b3d660f16f23cb2ffcaca0fb538257
Barry Tabios <b_tabios@yahoo.com>
2017-06-24 14:55:02 +1000

    jquery added updated background color on project panel

--- app.js
+++ app.js
@@ -3,3 +3,23 @@ console.log('JS Connected ... ');
 // document.querySelector('.date').innerHTML = Date();
 
 $(".date").html(Date());
+
+$(".nin-ten-doe").click(function(){
+  window.location.href ="https://bazzat10.github.io/Projects/"
+});
+
+$(".ellas-online-bookstore").click(function(){
+  window.location.href ="https://lit-bayou-99006.herokuapp.com/"
+});
+
+$(".tripr").click(function(){
+  window.location.href ="https://glacial-bayou-90913.herokuapp.com/"
+});
+
+$(".carpool").click(function(){
+  window.location.href ="https://mysterious-bastion-51155.herokuapp.com/"
+});
+
+$(".ga-online-bank").click(function(){
+  window.location.href ="https://bazzat10.github.io/ga_bank/"
+});
